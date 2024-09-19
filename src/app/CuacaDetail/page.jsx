7f694cc7b19e5180138d6c780bca2d33a1026497
IsfaFarhan <157976265+IsfaFarhan@users.noreply.github.com>
2024-09-19 19:01:10 +0800

    Fiz suspense

--- src/app/CuacaDetail/page.jsx
+++ src/app/CuacaDetail/page.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Cuaca2 from "../../ui components/state";
 import Cuaca3 from "../../ui components/district";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Cuaca() {
   const [addDay, setAddDay] = useState(6);
@@ -56,35 +58,37 @@ export default function Cuaca() {
   };
 
   return (
-    <div>
-      {/* z-50 relative bottom-14 left-[38rem] */}
-      <div className=" z-50 lg:absolute sm:static right-[2rem] top-5 flex-row flex items-center justify-center">
-        <button onClick={buttonAllDec}>
-          <Image
-            className="bg-[#46BCFF] rounded-tl-md rounded-bl-md hover:bg-[#2D7FAE] transition duration-300 ease-in-out"
-            src="/left chev.svg" // Path to your image
-            alt="Tolak hari" // Alternative text for accessibility
-            width={34} // Desired width
-            height={34}
-          />
-        </button>
-        <p className="text-center min-w-[97px] p-1 pr-4 pl-4 bg-white border border-[#2D7FAE] border-opacity-25">
-          {countDay}
-        </p>
-        <button onClick={buttonAllInc}>
-          <Image
-            className="bg-[#46BCFF] rounded-tr-md rounded-br-md hover:bg-[#2D7FAE] transition duration-300 ease-in-out"
-            src="/right chev.svg" // Path to your image
-            alt="Tolak hari" // Alternative text for accessibility
-            width={34} // Desired width
-            height={34}
-          />
-        </button>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        {/* z-50 relative bottom-14 left-[38rem] */}
+        <div className=" z-50 lg:absolute sm:static right-[2rem] top-5 flex-row flex items-center justify-center">
+          <button onClick={buttonAllDec}>
+            <Image
+              className="bg-[#46BCFF] rounded-tl-md rounded-bl-md hover:bg-[#2D7FAE] transition duration-300 ease-in-out"
+              src="/left chev.svg" // Path to your image
+              alt="Tolak hari" // Alternative text for accessibility
+              width={34} // Desired width
+              height={34}
+            />
+          </button>
+          <p className="text-center min-w-[97px] p-1 pr-4 pl-4 bg-white border border-[#2D7FAE] border-opacity-25">
+            {countDay}
+          </p>
+          <button onClick={buttonAllInc}>
+            <Image
+              className="bg-[#46BCFF] rounded-tr-md rounded-br-md hover:bg-[#2D7FAE] transition duration-300 ease-in-out"
+              src="/right chev.svg" // Path to your image
+              alt="Tolak hari" // Alternative text for accessibility
+              width={34} // Desired width
+              height={34}
+            />
+          </button>
+        </div>
+        <div className="w-screen min-w-[500px] flex-row flex justify-center flex-wrap">
+          <Cuaca2 addDay={addDay} />
+          <Cuaca3 addDay={addDay} />
+        </div>
       </div>
-      <div className="w-screen min-w-[500px] flex-row flex justify-center flex-wrap">
-        <Cuaca2 addDay={addDay} />
-        <Cuaca3 addDay={addDay} />
-      </div>
-    </div>
+    </Suspense>
   );
 }
